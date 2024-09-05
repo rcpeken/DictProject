9937763102397b585d5b74436e4be4684fe36fc7
rcpeken <recepeken113@gmail.com>
2024-09-06 00:33:22 +0300

    login api and register api completed

--- Enterance/Login.js
+++ Enterance/Login.js
@@ -1,141 +1,183 @@
-import { StyleSheet, Text, View, Image,ScrollView } from 'react-native';
-import { TextInput } from 'react-native-paper';
-import {useState} from 'react'
-import { Avatar } from 'react-native-paper';
-import { Button } from 'react-native-paper';
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { TextInput } from "react-native-paper";
+import { useState } from "react";
+import { Avatar } from "react-native-paper";
+import { Button } from "react-native-paper";
+import md5 from "js-md5";
+import axios from "axios";
 
+function Login() {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-function Login(){
-    const [text, setText] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordVisible, setPasswordVisible] = useState(false);
+  const handleInput = async () => {
+    try {
+      const secPassword = md5(password);
 
-return(
-    <ScrollView>
-    <View>
-        <View style={styles.avatar}>
-        <Avatar.Image size={84} source={require('../assets/icardi.png')} />
-        </View>
-    <View style={styles.title}>
-        <Text style={styles.text}>Login</Text>
-        <Text style={styles.text2}>Login to continue using the app </Text>
-         </View>
-         <View style={styles.textinput}>
-         <TextInput
-      mode="outlined"
-      label="Email"
-      placeholder="Enter Your Email"
-      value={text}
-      onChangeText={text => setText(text)}
-      //right={<TextInput.Affix text="/100" />}
-    />
-    <TextInput
-      mode="outlined"
-      label="Password"
-      placeholder="Enter Your Password"
-      secureTextEntry={!passwordVisible} // Şifre görünürlüğü
-        right={
-          <TextInput.Icon
-            icon={passwordVisible ? "eye-off" : "eye"} // Göz ikonu duruma göre değişir
-            onPress={() => setPasswordVisible(!passwordVisible)} // Görünürlüğü değiştirir
-          />
+      const response = await axios.post(
+        "http://64.225.109.88:8080/api/auth/login",
+        {
+          text,
+          password: secPassword,
         }
-      style={styles.input}
-      value={password}
-      onChangeText={password => setPassword(password)}
-    />
-    <Text style={styles.forgot}>Forgot Password?</Text>
-    </View>
-    <View style={styles.button}>
-    <Button mode="contained" style={[styles.button,{backgroundColor: '#48bee2'} ]} >
-    Login
-  </Button>
-  </View >
-  <View style={{marginTop:0 }}>
-  <Text style={{textAlign:'center',fontSize: 20,opacity: 0.5,paddingBottom: 0}}>Or Login With</Text>
-  <View style={styles.logoContainer}>
-  <Image source={require('../assets/google.png')} style={styles.logo}/>
-  <Image source={require('../assets/apple.png')} style={styles.logo}/>
-  </View>
-  </View>
-  <View style ={{flexDirection: 'row',justifyContent: 'center',alignItems: 'center'}}>
-  <Text>Dont have a account? </Text>
-  <Text style={{color: '#0b1ac4',fontWeight: 'bold'}}>Register </Text>
-  </View>
-   </View>
-   </ScrollView>
+      );
 
-    
-)} 
+      if (response.status === 200) {
+        console.log("Login successful:", response.data);
+      } else {
+        console.error("Login failed:", response.data.message);
+      }
+    } catch (error) {
+      console.error(
+        "An error occurred:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  return (
+    <ScrollView>
+      <View>
+        <View style={styles.avatar}>
+          <Avatar.Image size={84} source={require("../assets/icardi.png")} />
+        </View>
+        <View style={styles.title}>
+          <Text style={styles.text}>Login</Text>
+          <Text style={styles.text2}>Login to continue using the app </Text>
+        </View>
+        <View style={styles.textinput}>
+          <TextInput
+            mode="outlined"
+            label="Email"
+            placeholder="Enter Your Email"
+            value={text}
+            onChangeText={(text) => setText(text)}
+            //right={<TextInput.Affix text="/100" />}
+          />
+          <TextInput
+            mode="outlined"
+            label="Password"
+            placeholder="Enter Your Password"
+            secureTextEntry={!passwordVisible}
+            right={
+              <TextInput.Icon
+                icon={passwordVisible ? "eye-off" : "eye"}
+                onPress={() => setPasswordVisible(!passwordVisible)}
+              />
+            }
+            style={styles.input}
+            value={password}
+            onChangeText={(password) => setPassword(password)}
+          />
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </View>
+        <View style={styles.button}>
+          <Button
+            mode="contained"
+            style={[styles.button, { backgroundColor: "#48bee2" }]}
+            onPress={handleInput}
+          >
+            Login
+          </Button>
+        </View>
+        <View style={{ marginTop: 0 }}>
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              opacity: 0.5,
+              paddingBottom: 0,
+            }}
+          >
+            Or Login With
+          </Text>
+          <View style={styles.logoContainer}>
+            <Image
+              source={require("../assets/google.png")}
+              style={styles.logo}
+            />
+            <Image
+              source={require("../assets/apple.png")}
+              style={styles.logo}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>Dont have a account? </Text>
+          <Text style={{ color: "#0b1ac4", fontWeight: "bold" }}>
+            Register{" "}
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
 
 export default Login;
 
 const styles = StyleSheet.create({
-   
-    title:{
-        marginTop: 30,
-        margin: 5,
-        padding: 24,
-        borderColor: '#2e35bc',
-    },
+  title: {
+    marginTop: 30,
+    margin: 5,
+    padding: 24,
+    borderColor: "#2e35bc",
+  },
 
-    text: {
-        fontSize: 25,
-        //textAlignVertical: "center",
-        textAlign: "left",
-        fontWeight: 'bold',
-        
-    },
-    avatar:{
-        alignItems: 'center',
-        marginTop: 100,
-    },
-    text2:{
-        opacity: 0.5,
-    },
-    textinput:{
-        
-        margin: 20,
-        height: 70,
-        borderRadius: 150,
-        padding:10,
-        justifyContent:'space-between',
-        
+  text: {
+    fontSize: 25,
+    //textAlignVertical: "center",
+    textAlign: "left",
+    fontWeight: "bold",
+  },
+  avatar: {
+    alignItems: "center",
+    marginTop: 100,
+  },
+  text2: {
+    opacity: 0.5,
+  },
+  textinput: {
+    margin: 20,
+    height: 70,
+    borderRadius: 150,
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  button: {
+    borderRadius: 40,
+    width: 350,
+    padding: 10,
+    alignSelf: "center",
+    marginTop: 90,
+    margin: 24,
+  },
+  input: {
+    marginTop: 40,
+  },
+  forgot: {
+    padding: 3,
+    //flexDirection: 'column',
+    // marginLeft: 230
+    textAlign: "right",
+    fontSize: 15,
+  },
 
-    },
-    button:{
-        borderRadius:40,
-        width: 350,
-        padding: 10,
-        alignSelf: 'center',
-        marginTop:90,
-        margin: 24,
-        
-        
-    },
-    input:{
-        marginTop: 40,
-    },
-    forgot:{
-        padding: 3,
-        //flexDirection: 'column',
-       // marginLeft: 230
-       textAlign: 'right',
-       fontSize: 15,
-
-    },
-
-    logoContainer:{
-        flexDirection: 'row',  // Yatay düzen
-        justifyContent: 'center',  // Ortala
-        alignItems: 'center',  // Dikeyde ortala
-        marginVertical: 20,
-           
-    },
-    logo:{
-        width: 50,  
-        height: 50,  
+  logoContainer: {
+    flexDirection: "row", // Yatay düzen
+    justifyContent: "center", // Ortala
+    alignItems: "center", // Dikeyde ortala
+    marginVertical: 20,
+  },
+  logo: {
+    width: 50,
+    height: 50,
     marginHorizontal: 20,
-    }
-
-})
+  },
+});
--- Enterance/Register.js
+++ Enterance/Register.js
@@ -1,151 +1,165 @@
-import { StyleSheet, Text, View} from 'react-native';
-import { TextInput } from 'react-native-paper';
-import { Button } from 'react-native-paper';
-import { Avatar } from 'react-native-paper';
-import {useState} from 'react'
-
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+import { Button } from "react-native-paper";
+import { Avatar } from "react-native-paper";
+import { useState } from "react";
+import md5 from "js-md5";
+import axios from "axios";
 
 function Register() {
+  const [text, setText] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [passwordVisible0, setPasswordVisible0] = useState(false);
 
-    const [text, setText] = useState("");
-    const [password, setPassword] = useState("");
-    const [username, setUsername] = useState("");
-    const [confirmpassword, setConfirmpassword] = useState("");
-    const [passwordVisible, setPasswordVisible] = useState(false);
-    const [passwordVisible0, setPasswordVisible0] = useState(false);
+  const handleRegister = async () => {
+    try {
+      const secPassword = md5(password);
 
-    return(
-        <>
-        <View style={styles.container1}>
+      const response = await axios.post(
+        "http://64.225.109.88:8080/api/auth/UI32-234F-234Y",
+        {
+          text,
+          username,
+          password: secPassword,
+        }
+      );
+    } catch (error) {
+      console.error(
+        "An error occurred:",
+        error.response ? error.response.data : error.message
+      );
+    }
+  };
+
+  return (
+    <>
+      <View style={styles.container1}>
         <View style={styles.avatar}>
-        <Avatar.Image size={84} source={require('../assets/icardi.png')} />
-        </View >
-        <View style ={styles.textContainer}>
-        <Text style={styles.text}>Register</Text>
-        <Text style={styles.text2}>Enter Your Personal İnformation </Text>
+          <Avatar.Image size={84} source={require("../assets/icardi.png")} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>Register</Text>
+          <Text style={styles.text2}>Enter Your Personal İnformation </Text>
         </View>
         <View style={styles.container}>
-        <View style = {styles.textInput}>
+          <View style={styles.textInput}>
             <TextInput
-      mode="outlined"
-      label="Username"
-      placeholder="Enter Your Username"
-      value={username}
-      onChangeText={username => setUsername(username)}
-      //right={<TextInput.Affix text="/100" />}
-    />
-    <TextInput
-      mode="outlined"
-      label="Email"
-      placeholder="Enter Your Email"
-      value={text}
-      onChangeText={text => setText(text)}
-      //right={<TextInput.Affix text="/100" />}
-    />
-    <TextInput
-      mode="outlined"
-      label="Password"
-      placeholder="Enter Your Password"
-      secureTextEntry={!passwordVisible} // Şifre görünürlüğü
-        right={
-          <TextInput.Icon
-            icon={passwordVisible ? "eye-off" : "eye"} // Göz ikonu duruma göre değişir
-            onPress={() => setPasswordVisible(!passwordVisible)} // Görünürlüğü değiştirir
-          />
-        }
-      value={password}
-      onChangeText={password => setPassword(password)}
-      //right={<TextInput.Affix text="/100" />}
-    />
-    <TextInput
-      mode="outlined"
-      label="Confirm Password"
-      placeholder="Enter confirm password"
-      secureTextEntry={!passwordVisible0} // Şifre görünürlüğü
-        right={
-          <TextInput.Icon
-            icon={passwordVisible0 ? "eye-off" : "eye"} // Göz ikonu duruma göre değişir
-            onPress={() => setPasswordVisible0(!passwordVisible0)} // Görünürlüğü değiştirir
-          />
-        }
-      value={confirmpassword}
-      onChangeText={confirmpassword => setConfirmpassword(confirmpassword)}
-      
-    />
+              mode="outlined"
+              label="Username"
+              placeholder="Enter Your Username"
+              value={username}
+              onChangeText={(username) => setUsername(username)}
+              //right={<TextInput.Affix text="/100" />}
+            />
+            <TextInput
+              mode="outlined"
+              label="Email"
+              placeholder="Enter Your Email"
+              value={text}
+              onChangeText={(text) => setText(text)}
+              //right={<TextInput.Affix text="/100" />}
+            />
+            <TextInput
+              mode="outlined"
+              label="Password"
+              placeholder="Enter Your Password"
+              secureTextEntry={!passwordVisible}
+              right={
+                <TextInput.Icon
+                  icon={passwordVisible ? "eye-off" : "eye"}
+                  onPress={() => setPasswordVisible(!passwordVisible)}
+                />
+              }
+              value={password}
+              onChangeText={(password) => setPassword(password)}
+              //right={<TextInput.Affix text="/100" />}
+            />
+            <TextInput
+              mode="outlined"
+              label="Confirm Password"
+              placeholder="Enter confirm password"
+              secureTextEntry={!passwordVisible0}
+              right={
+                <TextInput.Icon
+                  icon={passwordVisible0 ? "eye-off" : "eye"}
+                  onPress={() => setPasswordVisible0(!passwordVisible0)}
+                />
+              }
+              value={confirmpassword}
+              onChangeText={(confirmpassword) =>
+                setConfirmpassword(confirmpassword)
+              }
+            />
+          </View>
+          <Button
+            mode="contained"
+            style={[styles.button, { backgroundColor: "#48bee2" }]}
+            onPress={handleRegister}
+          >
+            <Text style={{ fontSize: 18 }}> Register </Text>
+          </Button>
         </View>
-        <Button mode="contained" style={[styles.button,{backgroundColor: '#48bee2'},]} >
-        <Text style={{fontSize: 18,}}> Register </Text>
-      </Button>
       </View>
-      </View>
-      </>
-    )
-
+    </>
+  );
 }
 
 export default Register;
 
-
 const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+  },
 
-    container1:{
-        flex: 1,
-    },
+  container: {
+    //flex: 1,
+    //justifyContent : 'center',
+    //alignItems: 'center',
+    //marginTop: 50,
+    paddingTop: 20,
+  },
 
-    container:{
-        //flex: 1,
-        //justifyContent : 'center',
-        //alignItems: 'center',
-        //marginTop: 50,
-        paddingTop: 20,
-        
-    },
+  textInput: {
+    //marginTop: 100,
+    //height:  '70%',
+    margin: 20,
+    padding: 10,
+    gap: 50,
+    //flex: 1,
+    //justifyContent:'center',
+  },
+  button: {
+    borderRadius: 40,
+    width: 350,
+    padding: 10,
+    alignSelf: "center",
+    margin: 24,
+    marginTop: 50,
+  },
+  avatar: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 60,
+  },
+  text: {
+    fontSize: 25,
+    //textAlignVertical: "center",
+    //textAlign: "left",
 
-
-    textInput:{
-        //marginTop: 100,
-        //height:  '70%',
-        margin: 20,
-        padding: 10,
-        gap: 50,
-        //flex: 1,
-        //justifyContent:'center',
-
-           
-    },
-    button:{
-        borderRadius:40,
-        width: 350,   
-        padding:10,
-        alignSelf: 'center',
-        margin: 24,
-        marginTop: 50,
-    },
-    avatar:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 60,
-    },
-    text: {
-        fontSize: 25,
-        //textAlignVertical: "center",
-        //textAlign: "left",
-        
-        fontWeight: 'bold',
-        
-    },
-    text2: {
-        fontSize: 20,
-        opacity: 0.5
-    },
-    textContainer: {
-        //justifyContent : 'center',
-        //alignItems: 'center'
-        marginTop:5,
-        gap: 10,
-        margin:12,
-    },
-
-
-
-})
+    fontWeight: "bold",
+  },
+  text2: {
+    fontSize: 20,
+    opacity: 0.5,
+  },
+  textContainer: {
+    //justifyContent : 'center',
+    //alignItems: 'center'
+    marginTop: 5,
+    gap: 10,
+    margin: 12,
+  },
+});
